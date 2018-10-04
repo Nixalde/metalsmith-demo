9fe5ae7aa677c813e2481a6d0d45765ec1cd5314
Nixalde <43638580+Nixalde@users.noreply.github.com>
2018-10-04 13:32:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var Metalsmith  = require('metalsmith');
 var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
 var permalinks  = require('metalsmith-permalinks');
-#var serve = require('metalsmith-serve');
-#var watch = require('metalsmith-watch');
 
 Metalsmith(__dirname)
   .metadata({
@@ -20,17 +18,6 @@ Metalsmith(__dirname)
   .use(layouts({
     engine: 'handlebars'
   }))
-#.use(serve({
-#  host: "0.0.0.0",
-#  port: 8081,
-#  verbose: true
-#}))
-#.use(watch({
-#    paths: {
-#      "${source}/**/*": true,
-#      "layout/**/*": "**/*",
-#    }
-#  }))
   .build(function(err, files) {
     if (err) { throw err; }
   });
